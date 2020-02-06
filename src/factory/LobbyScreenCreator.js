bcb02434e1bd1209d3627d19ff32b869118a0bfe
IkeyBenz <ikey.benz@gmail.com>
2020-02-06 02:29:06 -0800

    [add] now the share lobby message is customized with current lobby Id

--- src/factory/LobbyScreenCreator.js
+++ src/factory/LobbyScreenCreator.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Share from 'react-native-share';
 import { LobbyScreen } from '../ui';
 
 const LobbyScreenCreator = ({ navigation }) => {
+  // Use lobbyId from url as 'Game Pin'
   const gamePin = navigation.getParam('id') || 123456;
+
+  // Connect to LobbyService
   const lobbyCreator = { username: 'ikey', id: 2 };
   const currentPlayer = { username: 'ikey', id: 2 };
   const connectedPlayers = [
@@ -11,8 +14,18 @@ const LobbyScreenCreator = ({ navigation }) => {
     { username: 'jshapoopa 42', id: 2 },
     { username: 'louie', id: 3 },
   ];
-  const openShareTab = () => console.log('Opened share tab');
+
+  // When lobby creator presses 'start game' button
   const goToGame = () => console.log('Navigating to game!');
+
+  // When user presses 'Invite Friends' button
+  const openShareTab = () =>
+    Share.open({
+      message: `Ikey invited you to join their Modi Game! com.ikeybenz.modi:lobbies/${gamePin}`,
+    })
+      .then(res => console.log('Res:', res))
+      .catch(err => console.log('Err', err));
+
   return (
     <LobbyScreen
       gamePin={gamePin}
