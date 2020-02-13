17e99270bbbd532aa37efdb4df3ef7efcc2cd454
IkeyBenz <ikey.benz@gmail.com>
2020-02-13 11:07:03 -0800

    [polish] added username input to lobbyscreen on empty username + made layout more responsive

--- src/factory/LobbyScreenCreator.js
+++ src/factory/LobbyScreenCreator.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import Share from 'react-native-share';
 
 import useLobbyConnection from '../hooks/useLobbyConnection';
@@ -6,25 +6,20 @@ import { LobbyScreen } from '../ui';
 import AppContext from '../StateManager';
 
 const LobbyScreenCreator = ({ navigation }) => {
-  const [appState, _] = useContext(AppContext);
-  const lobbyId = navigation.getParam('id'),
-    gamePin = lobbyId;
-  const [username, setUsername] = useState(appState.username);
-
-  // If came from url, manually set username here
-  useEffect(() => {
-    if (!username) {
-      setUsername(prompt('Enter username:'));
-    }
-  }, [username]);
-
-  const { connectedPlayers, currentPlayer, lobbyLeader } = useLobbyConnection(
-    lobbyId,
-    username,
-  );
+  const lobbyId = navigation.getParam('id');
+  const [{ username }, updateState] = useContext(AppContext);
+  const {
+    connectedPlayers,
+    currentPlayer,
+    lobbyLeader,
+    sendMessage,
+  } = useLobbyConnection(lobbyId, username);
 
   // When lobby creator presses 'start game' button
-  const goToGame = () => console.log('Navigating to game!');
+  const goToGame = () => {
+    console.log('(not implemented yet) leader started game!');
+    // sendMessage('start game');
+  };
 
   // When user presses 'Invite Friends' button
   const openShareTab = () =>
@@ -34,10 +29,12 @@ const LobbyScreenCreator = ({ navigation }) => {
 
   return (
     <LobbyScreen
-      gamePin={gamePin}
+      gamePin={lobbyId}
       lobbyCreator={lobbyLeader}
       currentPlayer={currentPlayer}
       connectedPlayers={connectedPlayers}
+      askForUsername={!username || username === ''}
+      onUsernameSet={u => updateState({ username: u })}
       onInviteFriendsBtnPressed={openShareTab}
       onStartGameBtnPressed={goToGame}
       onBackBtnPressed={navigation.goBack}
--- src/ui/screens/LobbyScreen.js
+++ src/ui/screens/LobbyScreen.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
 
-import { ScreenContainer, Container, Button, Text } from '../components';
+import {
+  ScreenContainer,
+  Container,
+  Button,
+  Text,
+  TextInput,
+} from '../components';
 import { BackIcon } from '../icons';
 
 const LobbyScreen = ({
@@ -9,13 +16,15 @@ const LobbyScreen = ({
   lobbyCreator,
   currentPlayer,
   connectedPlayers,
+  askForUsername,
+  onUsernameSet,
   onInviteFriendsBtnPressed,
   onStartGameBtnPressed,
   onBackBtnPressed,
 }) => (
   <ScreenContainer>
-    <Container flex={1} padding={16}>
-      <Container flex={2} justifyContent="center">
+    <Container flex={1} padding={8} paddingHorizontal={16}>
+      <Container flex={2} justifyContent="center" minHeight={52}>
         <Container alignItems="center">
           <Text fontSize={24}>Game PIN:</Text>
           <Text fontSize={42}>{gamePin}</Text>
@@ -29,12 +38,13 @@ const LobbyScreen = ({
         <PlayerList players={connectedPlayers} />
       </Container>
 
-      <Container flex={1} flexDirection="row" alignItems="center">
+      <Container
+        flex={1}
+        flexDirection="row"
+        alignItems="center"
+        minHeight={24}>
         <Container flex={1} marginRight={8}>
-          <Button
-            bgColor="red"
-            onPress={onBackBtnPressed}
-            paddingHorrizontal={16}>
+          <Button bgColor="red" onPress={onBackBtnPressed}>
             <BackIcon size={28} />
           </Button>
         </Container>
@@ -52,6 +62,18 @@ const LobbyScreen = ({
         </Container>
       </Container>
     </Container>
+    {askForUsername && (
+      <Container>
+        <TextInput
+          placeholder="Username"
+          padding={8}
+          fontSize={28}
+          marginHorizontal={16}
+          onSubmitEditing={e => onUsernameSet(e.nativeEvent.text)}
+        />
+        <KeyboardSpacer />
+      </Container>
+    )}
   </ScreenContainer>
 );
 
