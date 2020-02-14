dd3e63c9b4f3adb196fa1cefdb2aa171ac2824e7
IkeyBenz <ikey.benz@gmail.com>
2020-02-14 14:27:46 -0800

    [refactor] moved useLobbyConnection hook into LobbyService

--- src/service/LobbyService.js
+++ src/service/LobbyService.js
@@ -1,4 +1,6 @@
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import io from 'socket.io-client';
 
 // const API_URL = 'https://modi-server.herokuapp.com';
 const API_URL = 'http://localhost:5000';
@@ -21,6 +23,35 @@ class LobbyService {
 
   async isLobbyIdValid(id) {
     return !!(await this.findLobbyById(id));
+  }
+
+  useLobbyConnection(lobbyId, username) {
+    const [lobbyInfo, setLobbyInfo] = useState({
+      connectedPlayers: [],
+      currentPlayer: {},
+      lobbyLeader: {},
+    });
+    const sendMessage = useRef();
+    useEffect(() => {
+      const socket = io(`${API_URL}/lobbies/${lobbyId}?username=${username}`);
+
+      socket.on('connect', () => {
+        if (!sendMessage.current) {
+          sendMessage.current = socket.send;
+        }
+        socket.on('lobby info', ({ connectedPlayers, lobbyLeader }) =>
+          setLobbyInfo({
+            currentPlayer: connectedPlayers.find(p => p.id === socket.id),
+            connectedPlayers,
+            lobbyLeader,
+          }),
+        );
+      });
+
+      return () => socket.disconnect();
+    }, [lobbyId, username]);
+
+    return { ...lobbyInfo, sendMessage: sendMessage.current };
   }
 }
 
