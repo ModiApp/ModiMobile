e9a99cd6b7b8da78961006b4c773cd243084f16e
IkeyBenz <ikey.benz@gmail.com>
2020-02-14 14:16:17 -0800

    [refactor] moved creating lobby logic into LobbyService and joining lobby logic into JoinLobbyScreen modal

--- src/factory/HomeScreenCreator.js
+++ src/factory/HomeScreenCreator.js
@@ -1,35 +1,21 @@
 import React, { useState, useContext } from 'react';
-import axios from 'axios';
 
 import AppContext from '../StateManager';
+import { LobbyService } from '../service';
 import { HomeScreen } from '../ui';
 
-const API_URL = 'https://modi-server.herokuapp.com';
-//const API_URL = 'http://localhost:5000';
-
-const createLobby = () =>
-  axios.get(`${API_URL}/lobbies/new`).then(r => r.data.lobbyId);
-// const createLobby = () => new Promise(r => setTimeout(() => r('1234'), 500));
 const HomeScreenCreator = ({ navigation }) => {
   const [state, updateState] = useContext(AppContext);
   const [isCreatingGame, setIsCreatingGame] = useState(false);
   const [isJoiningGame, setIsJoiningGame] = useState(false);
   const [shouldAskForUsername, setShouldAskForUsername] = useState(false);
+
   const onCreateGameButtonPressed = async () => {
     if (requireUsername()) {
       setIsCreatingGame(true);
-      createLobby()
-        .then(id => {
-          navigation.navigate('Lobby', { id });
-        })
+      LobbyService.createLobby()
+        .then(id => navigation.navigate('Lobby', { id }))
         .finally(() => setIsCreatingGame(false));
-    }
-  };
-  const onJoinGameButtonPressed = () => {
-    if (requireUsername()) {
-      setIsJoiningGame(true);
-      // Modal with prompt to set lobbyId
-      navigation.navigate('JoinGame');
     }
   };
 
@@ -41,12 +27,17 @@ const HomeScreenCreator = ({ navigation }) => {
   return (
     <HomeScreen
       onCreateGameBtnPressed={onCreateGameButtonPressed}
-      onJoinGameBtnPressed={onJoinGameButtonPressed}
+      onJoinGameBtnPressed={() => {
+        if (requireUsername()) {
+          navigation.navigate('JoinLobby');
+          setIsJoiningGame(true);
+        }
+      }}
       onUsernameUpdated={u => updateState({ username: u })}
       username={state.username}
       isCreatingGame={isCreatingGame}
       isJoiningGame={isJoiningGame}
-      shouldAskForUsername={shouldAskForUsername && state.username === ''}
+      shouldAskForUsername={shouldAskForUsername && !!state.username}
     />
   );
 };
