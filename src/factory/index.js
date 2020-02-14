701d68fc0b83f83276cbdf508ffc5ab04a647546
IkeyBenz <ikey.benz@gmail.com>
2020-02-14 14:18:01 -0800

    [add] JoinLobbyScreen

--- src/factory/index.js
+++ src/factory/index.js
@@ -1,4 +1,5 @@
 import HomeScreenCreator from './HomeScreenCreator';
 import LobbyScreenCreator from './LobbyScreenCreator';
+import JoinLobbyScreenCreator from './JoinLobbyScreenCreator';
 
-export { HomeScreenCreator, LobbyScreenCreator };
+export { HomeScreenCreator, LobbyScreenCreator, JoinLobbyScreenCreator };
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,4 +1,5 @@
 import HomeScreen from './screens/HomeScreen';
+import JoinLobbyScreen from './screens/JoinLobbyScreen';
 import LobbyScreen from './screens/LobbyScreen';
 
-export { HomeScreen, LobbyScreen };
+export { HomeScreen, JoinLobbyScreen, LobbyScreen };
