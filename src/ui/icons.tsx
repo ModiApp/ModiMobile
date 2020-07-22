138066fab420f2576526ef62af5389eae3bb4ce2
IkeyBenz <ikey.benz@gmail.com>
2020-07-22 17:33:34 -0400

    [cleanup] make a custom IconProps interface to be used by all future Icon wrappers

--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export const BackIcon: React.FC<{ size: number }> = ({ size }) => (
+interface IconProps {
+  size: number;
+}
+
+export const BackIcon: React.FC<IconProps> = ({ size }) => (
   <Icon
     size={size}
     name={Platform.OS === 'ios' ? 'ios-arrow-back' : 'md-arrow-round-back'}
