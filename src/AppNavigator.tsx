1e01f8152a872228ce942840a15c72e8f6f181fc
IkeyBenz <ikey.benz@gmail.com>
2020-07-22 17:35:17 -0400

    [patch] reset initial route name to Home

--- src/AppNavigator.tsx
+++ src/AppNavigator.tsx
@@ -26,7 +26,7 @@ const AppStack = createStackNavigator(
     },
   },
   {
-    initialRouteName: 'Game',
+    initialRouteName: 'Home',
     defaultNavigationOptions: {
       headerShown: false,
     },
