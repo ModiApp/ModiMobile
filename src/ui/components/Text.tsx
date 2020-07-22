7e509ff39406828ab157850cbd0e59f2cbd6ac57
IkeyBenz <ikey.benz@gmail.com>
2020-07-22 16:15:24 -0400

    [add] custom Button and Text components. The old custom components were fundamentally difficult to use with strict types, and styles with props is unconventional

---
+++ src/ui/components/Text.tsx
@@ -0,0 +1,27 @@
+import React, { useRef } from 'react';
+import { Text as RNText, StyleProp, TextStyle, ViewStyle } from 'react-native';
+
+import { colors } from '../styles';
+
+interface TextProps {
+  /** Defaults to 14 */
+  size?: number;
+
+  /** Defaults to white */
+  color?: ColorName;
+
+  /** Optional override styles */
+  style?: StyleProp<TextStyle>;
+}
+
+const Text: React.FC<TextProps> = ({ size, children, style, color }) => {
+  const styles = useRef<StyleProp<TextStyle>>({
+    fontSize: size || 18,
+    fontFamily: 'Chalkduster',
+    color: colors[color || 'white'],
+  }).current;
+
+  return <RNText style={[styles, style]}>{children}</RNText>;
+};
+
+export default Text;
