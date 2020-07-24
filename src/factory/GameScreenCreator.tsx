4c43f29d0b288ca3e8d314e22769c720cfcf9fd2
IkeyBenz <ikey.benz@gmail.com>
2020-07-24 19:03:35 -0400

    [refactor] GameScreenCreator to use the GameStateProvider to get game state to the GameScreen

--- src/factory/GameScreenCreator.tsx
+++ src/factory/GameScreenCreator.tsx
@@ -1,71 +1,19 @@
-import React, { useEffect, useContext, useReducer } from 'react';
-import { Animated } from 'react-native';
-import {
-  NavigationScreenProp,
-  NavigationScreenComponent,
-} from 'react-navigation';
+import React, { useEffect, useContext } from 'react';
+import { NavigationStackScreenComponent } from 'react-navigation-stack';
 
 import { GameScreen } from '../ui';
-import GameService, { createMockGameService } from '../service/GameService';
+import GameStateProvider from '../service/GameStateProvider';
 import AppContext from '../StateManager';
 
-const initialGameState: ModiGameState = {
-  round: 0,
-  dealerId: '',
-  activePlayerIdx: 0,
-  playersCards: new Map(),
-  moves: [],
-  liveCounts: [],
-  cardOrder: [1, 2, 3, 4, 5, 6],
-  playerOrder: [],
-};
+// Not yet sure if i want to make a regular http request or use socket
+// to determine if game exists
+const mockGameService = () => ({
+  isGameIdValid: (id: string) => !!id,
+});
 
-const gameStateReducer: React.Reducer<ModiGameState, GameStateChangeAction> = (
-  state,
-  action,
-) => {
-  switch (action.type) {
-    case 'set entire state':
-      return { ...action.payload };
-    case 'card order changed':
-      return { ...state, cardOrder: action.payload };
-    case 'live counts changed':
-      return { ...state, liveCounts: action.payload };
-    case 'player order changed':
-      return { ...state, playerOrder: action.payload };
-    default:
-      return state;
-  }
-};
+const GameService = mockGameService();
 
-const GameScreenCreator: React.FC<{
-  gameId: string;
-  username: string;
-  authorizedPlayerId: string;
-}> = ({ gameId, username, authorizedPlayerId }) => {
-  const [gameState, dispatchGameStateChangeAction] = useReducer(
-    gameStateReducer,
-    initialGameState,
-  );
-
-  useEffect(() => {
-    const gameConnection = createMockGameService({
-      gameId,
-      authorizedPlayerId,
-      username,
-      onGameStateChanged: dispatchGameStateChangeAction,
-    });
-    return () => gameConnection.disconnect();
-  }, []);
-
-  return <GameScreen gameState={gameState} />;
-};
-
-export default ({
-  navigation,
-}: {
-  navigation: NavigationScreenProp<{}, { id: string }>;
-}) => {
+const GameScreenCreator: NavigationStackScreenComponent = ({ navigation }) => {
   const [globalState, updateGlobalState] = useContext(AppContext);
   const { username, authorizedPlayerId } = globalState;
   const gameId = navigation.getParam('id');
@@ -83,12 +31,13 @@ export default ({
   }, [gameId]);
 
   return (
-    <GameScreenCreator
-      gameId={gameId}
+    <GameStateProvider
       username={username!}
       authorizedPlayerId={authorizedPlayerId!}
-    />
+      gameId={gameId}>
+      <GameScreen />
+    </GameStateProvider>
   );
 };
 
-// const createGameScreenWithCredentials
+export default GameScreenCreator;
