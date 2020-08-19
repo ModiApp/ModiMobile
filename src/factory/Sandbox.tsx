a62d0433514dae20e6c841cd86d57970080a896d
IkeyBenz <ikey.benz@gmail.com>
2020-08-19 02:04:32 -0400

    [add] sandbox screen in dev mode. to be used to create new components.

---
+++ src/factory/Sandbox.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavigationStackScreenComponent } from 'react-navigation-stack';
+import { ScreenContainer } from 'src/ui/components';
+
+const SandboxScreen: NavigationStackScreenComponent = () => {
+  return <ScreenContainer />;
+};
+
+export default SandboxScreen;
--- src/factory/index.tsx
+++ src/factory/index.tsx
@@ -3,7 +3,9 @@ import LobbyScreenCreator from './LobbyScreenCreator';
 import JoinLobbyScreenCreator from './JoinLobbyScreenCreator';
 import GameScreenCreator from './GameScreenCreator';
 
+// Only enabled in dev mode:
 import TestGameScreenLoading from './TestGameScreenLoading';
+import SandboxScreen from './Sandbox';
 
 export {
   HomeScreenCreator,
@@ -11,4 +13,5 @@ export {
   JoinLobbyScreenCreator,
   GameScreenCreator,
   TestGameScreenLoading,
+  SandboxScreen,
 };
